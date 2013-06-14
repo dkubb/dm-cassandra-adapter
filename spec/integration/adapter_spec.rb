8c08d5a9dbf5f919a1409c78bc84802155f8770c
Dan Kubb <dan.kubb@gmail.com>
2013-06-14 15:50:10 -0700

    Update specs to test UUID and Serial integer keys

--- spec/integration/adapter_spec.rb
+++ spec/integration/adapter_spec.rb
@@ -15,33 +15,65 @@ describe 'DataMapper::Adapters::CassandraAdapter' do
     retry
   end
 
-  def create_table
-    adapter.execute('CREATE TABLE heffalumps (id timeuuid PRIMARY KEY, color text, num_spots int, striped boolean)')
-  end
-
   # Use methods to avoid let() in before(:all) warnings
   def adapter() DataMapper::Spec.adapter end
   def repository() DataMapper.repository(adapter.name) end
 
-  # Define a custom model with a UUID PK
-  def heffalump_model
-    @model ||= Class.new {
-      include DataMapper::Resource
+  context 'with a generated UUID key' do
+    # Define a table with a UUID key
+    def create_table
+      adapter.execute('CREATE TABLE heffalumps (id timeuuid PRIMARY KEY, color text, num_spots int, striped boolean)')
+    end
 
-      property :id,        DataMapper::Property::SimpleUUID
-      property :color,     DataMapper::Property::String
-      property :num_spots, DataMapper::Property::Integer
-      property :striped,   DataMapper::Property::Boolean
+    # Define a custom model with a UUID key
+    def heffalump_model
+      @model ||= Class.new {
+        include DataMapper::Resource
 
-      # This is needed for DataMapper.finalize
-      def self.name() 'Heffalump' end
-    }.tap { DataMapper.finalize }
+        property :id,        DataMapper::Property::SimpleUUID
+        property :color,     DataMapper::Property::String
+        property :num_spots, DataMapper::Property::Integer
+        property :striped,   DataMapper::Property::Boolean
+
+        # This is needed for DataMapper.finalize
+        def self.name() 'Heffalump' end
+      }.tap { DataMapper.finalize }
+    end
+
+    before :all do
+      setup_keyspace
+      create_table
+    end
+
+    it_should_behave_like 'An Adapter'
   end
 
-  before :all do
-    setup_keyspace
-    create_table
-  end
+  context 'with a generated Integer key' do
+    # Define a table with an Integer key
+    def create_table
+      adapter.execute('CREATE TABLE heffalumps (id bigint PRIMARY KEY, color text, num_spots int, striped boolean)')
+    end
 
-  it_should_behave_like 'An Adapter'
+    # Define a custom model with an Integer key
+    def heffalump_model
+      @model ||= Class.new {
+        include DataMapper::Resource
+
+        property :id,        DataMapper::Property::Serial
+        property :color,     DataMapper::Property::String
+        property :num_spots, DataMapper::Property::Integer
+        property :striped,   DataMapper::Property::Boolean
+
+        # This is needed for DataMapper.finalize
+        def self.name() 'Heffalump' end
+      }.tap { DataMapper.finalize }
+    end
+
+    before :all do
+      setup_keyspace
+      create_table
+    end
+
+    it_should_behave_like 'An Adapter'
+  end
 end
