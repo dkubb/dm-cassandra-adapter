b8fde1fcfb2f62efe06c31153c29f59125560d0b
Justin Bradford <jabradford@gmail.com>
2014-03-17 16:45:42 -0700

    Add integration test for aggregate command

---
+++ spec/integration/command/aggregate_spec.rb
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+ENV['ADAPTER'] = 'cassandra'
+
+describe 'aggregate support' do
+  before :all do
+    setup_keyspace
+    adapter.execute("""
+      CREATE TABLE heffalumps (
+        user varint,
+        type varint,
+        time timestamp,
+        PRIMARY KEY (user, type, time)
+      )
+    """)
+  end
+
+  let(:model) do
+    Class.new {
+      include DataMapper::Resource
+      property :user, Integer,  key: true
+      property :type, Integer,  key: true
+      property :time, DateTime, key: true
+      # This is needed for DataMapper.finalize
+      def self.name() 'Heffalump' end
+    }.tap { DataMapper.finalize }
+  end
+
+  subject { described_class }
+
+  describe '#count' do
+    before :all do
+      model.count.should eq 0
+      model.create(user: 1, type: 1, time: Time.now)
+      model.create(user: 1, type: 2, time: Time.now)
+      model.create(user: 2, type: 1, time: Time.now)
+    end
+
+    it 'returns the number of rows' do
+      model.count.should eq 3
+    end
+
+    describe 'with conditions' do
+      it 'returns the number of matching rows' do
+        model.all(user: 1).count.should eq 2
+        model.all(user: 1, type: 2).count.should eq 1
+      end
+    end
+  end
+end
